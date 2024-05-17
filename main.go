06c21993949f0b69db2a1224667ea7f79bad65d4
James Chen <jameschen09@yahoo.com>
2024-05-16 19:52:18 -0700

    connected to DB

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	utils "github.com/jameschenn/smashbrosgo-api/utils"
+)
+
+func init() {
+	utils.LoadEnvVariables()
+	utils.ConnectToDB()
+}
 
 func main() {
 	fmt.Println("Hello Gopher James")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
