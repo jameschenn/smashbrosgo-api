3797a300d482dffdad6bc83a0d5a6354b993a812
James Chen <jameschen09@yahoo.com>
2024-06-04 18:05:12 -0700

    CRUD done

--- controllers/charactersController.go
+++ controllers/charactersController.go
@@ -56,3 +56,93 @@ func CharacterCreate(c *gin.Context) {
 		"character": character,
 	})
 }
+
+func GetAllCharacters(c *gin.Context) {
+
+	//Get all characters
+	var characters []models.Character
+	utils.DB.Find(&characters)
+
+	// Return array of all characters
+	c.JSON(200, gin.H{
+		"characters": characters,
+	})
+}
+
+func GetCharacterByID(c *gin.Context) {
+
+	//Get id from request body
+	id := c.Param("id")
+
+	//Get all characters
+	var character models.Character
+	utils.DB.First(&character, id)
+
+	// Returns characters
+	c.JSON(200, gin.H{
+		"character": character,
+	})
+}
+
+func UpdateCharacterByID(c *gin.Context) {
+
+	// Get id from request body
+	id := c.Param("id")
+
+	// Get data off request body
+	var payload struct {
+		Name            string
+		Series          string
+		FirstAppearance string
+		Description     string
+		Tier            string
+		Weight          float64
+		Speed           float64
+		NeutralB        string
+		UpB             string
+		SideB           string
+		DownB           string
+		Final_Smash     string
+		Image_URL       string
+	}
+
+	c.Bind(&payload)
+
+	//Find the character we're updating
+	var character models.Character
+	utils.DB.First(&character, id)
+
+	//Update the character
+	utils.DB.Model(&character).Updates(models.Character{
+		Name:            payload.Name,
+		Series:          payload.Series,
+		FirstAppearance: payload.FirstAppearance,
+		Description:     payload.Description,
+		Tier:            payload.Tier,
+		Weight:          payload.Weight,
+		Speed:           payload.Speed,
+		NeutralB:        payload.NeutralB,
+		UpB:             payload.UpB,
+		SideB:           payload.SideB,
+		DownB:           payload.DownB,
+		Final_Smash:     payload.Final_Smash,
+		Image_URL:       payload.Image_URL,
+	})
+
+	//Return character
+	c.JSON(200, gin.H{
+		"character": character,
+	})
+}
+
+func DeleteCharacterByID(c *gin.Context) {
+
+	// Get id from request body
+	id := c.Param("id")
+
+	//Delete the post
+	utils.DB.Delete(&models.Character{}, id)
+
+	//Respond
+	c.Status(200)
+}
--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func init() {
 func main() {
 	router := gin.Default()
 
+	router.GET("/characters", controllers.GetAllCharacters)
+	router.GET("/characters/:id", controllers.GetCharacterByID)
 	router.POST("/characters", controllers.CharacterCreate)
+	router.PUT("/characters/:id", controllers.UpdateCharacterByID)
+	router.DELETE("characters/:id", controllers.DeleteCharacterByID)
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
